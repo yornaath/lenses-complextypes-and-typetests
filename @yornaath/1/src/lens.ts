ad0dcf74f82636229cdb9d97b7b091c59e034d22
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-05-24 13:16:41 +0200

    some comments

--- @yornaath/1/src/lens.ts
+++ @yornaath/1/src/lens.ts
@@ -11,13 +11,21 @@ export type Lens<T> = {
   * A key path is a string that represents a path to a nested property in the object.
   * 
   * This type recursively traverses the keys in the object type T.
-  * It constructs a union of the current key K and recursively calls itself on the nested type of key K.
-  * If a key K points to a nested object, the key path is extended with dot notation.
+  *   -> It constructs a union of the current key K and recursively calls itself on the nested type of key K.
+  *   -> If a key K points to a nested object, the key path is extended with dot notation.
  */
 export type KeyPaths<T, K extends keyof T = keyof T> = K extends string | number
   ? K | `${K}${T[K] extends Record<any, any> ? `.${KeyPaths<T[K]> }` : ""}`
   : never;
 
+/**
+ * Extracts a specific nested type from an object based on a given key path.
+ * The key path represents a string that points to the desired nested property within the object.
+ * 
+ * This type traverses the object and recursively matches the key path to retrieve the nested type.
+ * It ensures type safety by checking the validity of each key in the path.
+ * If the key path is valid, it returns the corresponding nested type; otherwise, it evaluates to the 'never' type.
+ */
 export type TypePrism<
   T,
   P extends KeyPaths<T> = KeyPaths<T>
@@ -30,4 +38,3 @@ export type TypePrism<
     : never : never;
 
 
-    
