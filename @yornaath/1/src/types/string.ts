c79f8c8e5df7e479643d863af152bfa26dbb5f53
Jørn Andre Tangen @gorillatron <jornandretangen@gmail.com>
2023-06-06 13:16:35 +0200

    added SplitUnion

--- @yornaath/1/src/types/string.ts
+++ @yornaath/1/src/types/string.ts
@@ -6,11 +6,22 @@ import { Equals, Spec } from "./type-tests";
  *  -> If the delimiter is empty, it returns a tuple of characters.
  *    -> Otherwise, recursively splits the string into a tuple of strings.
  */
-export type Split<S extends string, D extends string> = S extends ""
-  ? []
-  : S extends `${infer T}${D}${infer U}`
-  ? [T, ...Split<U, D>]
-  : [S];
+export type Split<S extends string, D extends string> = 
+  S extends "" ? 
+    [] : 
+  S extends `${infer T}${D}${infer U}` ? 
+    [T, ...Split<U, D>] : 
+  [S];
+
+/**
+ * Splits a string into a union of strings based on a given delimiter.
+ */
+export type SplitUnion<S extends string, D extends string> = 
+  S extends "" ? 
+    "" :
+  S extends `${infer T}${D}${infer U}` ? 
+    T | SplitUnion<U, D> : 
+  S;
 
 /**
  * --------------------------
@@ -18,12 +29,22 @@ export type Split<S extends string, D extends string> = S extends ""
  * --------------------------
  */
 
-type Tests = Spec<
+type SplitTests = Spec<
   [
-    Equals<Split<"a.b.c", ".">, ["a", "b", "c"]>,
     Equals<Split<"", ".">, []>,
+    Equals<Split<"a.b.c", ".">, ["a", "b", "c"]>,
     Equals<Split<"a.b.c", "">, ["a", ".", "b", ".", "c"]>,
     Equals<Split<"a-b-c-d", "-">, ["a", "b", "c", "d"]>,
-    Equals<Split<"abcd", "">, ["a", "b", "c", "d"]>
+    Equals<Split<"abcd", "">, ["a", "b", "c", "d"]>,
+    Equals<Split<"a", ".">, ["a"]>,
+  ]
+>;
+
+type SplitUTests = Spec<
+  [
+    Equals<SplitUnion<"", ".">, "">,
+    Equals<SplitUnion<"a.b.c", ".">, "a" | "b" | "c">,
+    Equals<SplitUnion<"a-b-c-d", "-">, "a" | "b" | "c" | "d">,
+    Equals<SplitUnion<"a", ".">, "a">,
   ]
 >;
